00444bca9cdee1823392ce5024a3531249223ddd
Rafa-progz <rafa.trabajo@outlook.es>
2025-08-20 00:24:46 +0000

    hello

--- src/api/models.py
+++ src/api/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
+from flask_bcrypt import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -8,7 +9,12 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password).decode('utf-8')
+
+    def check_password(self,password):
+        return check_password_hash(self.password_hash, password)
 
 
     def serialize(self):
@@ -17,3 +23,4 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +21,61 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+
+    if not data ('email') or not ('password'):
+        return jsonify({'msg' : 'email and password are requiered'}), 400
+    
+    existing_user = db.session.execute (db.select(User).where(
+        User.email == data('email')
+    )).scalar_one_or_none()
+
+    if existing_user:
+        return jsonify({'msg' : 'user with this email already exist'}), 400
+    
+    new_user = User(email = data['email'])
+    new_user.set_password(data['password'])
+    db.session.add(new_user)
+    db.sesseion.commit()
+
+    return jsonify({'msg' : 'User created successfully'}), 201
+
+@api.route('/loging', method = ['POST'])
+def login():
+    data = request.get_json()
+
+    if not data ('email') or not ('password'):
+        return jsonify({'msg' : 'email and password are requiered'}), 400
+
+    user = db.session.execute (db.select(User).where(
+        User.email == data('email')
+    )).scalar_one_or_none()
+
+    if user is None:
+        return jsonify({'msg' : 'invalid email or password' }), 401
+    
+    if user.check_password(data ['password']):
+        return jsonify ({'msg' : login successful}) , 200
+    else:
+        return jsonify({'msg' : 'invalid email or password'})
+    
+    access_token = create_access_token(identity=user.id)
+    return jsonify({"access_token": access_token, "user_id": user.id}), 200
+
+
+@api.route("/private", methods=["GET"])
+@jwt_required()
+def private():
+    user_id = get_jwt_identity()
+    user = db.session.get(User, user_id)
+
+    if user is None:
+        return jsonify({"msg": "user not found"}), 404
+
+    return jsonify({"msg": "This is a private route", "email": user.email}), 200
+   
+
+
